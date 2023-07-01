73a006db664ec830f6dcea58ee702a3bcc496d1f
Harsh <115858346+harshprajapati460@users.noreply.github.com>
2023-07-02 00:02:34 +0530

    Updated

---
+++ graoh_matric.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n,m;
+cin >> n >> m;
+int adj[n+1][m+1];
+for(int i=0; i<n; i++){
+    int u,v;
+    cin >> u >> v;
+    adj[u][v] = 1;
+    adj[v][u] = 1;
+}
+for(int i=0 ; i<=n; i++){
+  for(int j=0; j<=m ; j++){
+   if(adj[i][j] != 1) adj[i][j] = 0;
+  }
+  
+}
+for(int i=0 ; i<=n; i++){
+  for(int j=0; j<=m ; j++){
+    cout<<adj[i][j]<<" ";
+  }
+  cout<<endl;
+}
+
+return 0;
+}
